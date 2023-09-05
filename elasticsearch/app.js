87297a041212375753ed77829adcc86d1e0a5e3f
khaled <elkotkhaled802@gmail.com>
2023-09-05 20:08:46 +0300

    basic setaup for grpc server

--- elasticsearch/app.js
+++ elasticsearch/app.js
@@ -3,6 +3,12 @@
 const { client } = require("./client");
 const { productNormalProperties, products } = require("./temp");
 const express = require("express");
+const path = require("path");
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+const packageDefinition = protoLoader.loadSync(
+    path.join(__dirname, "../protos/products.proto")
+);
 const {
     make_indexing,
     addMapToIndex,
@@ -10,36 +16,67 @@ const {
     insertDoc,
     isExistIndex,
 } = require("./es");
+const productsProto = grpc.loadPackageDefinition(packageDefinition);
 
-const app = express();
-
-
-app.get('/products', async (req, res) => {
-    const text = req.query.text;
-    const result = await basicSearch('products', {
-        query: {
-            match: {
-                name: text,
+async function findProduct(call, callback) {
+    const q = call.request.q;
+    const result = (
+        await basicSearch("products", {
+            query: {
+                match: {
+                    name: q,
+                },
             },
-        }
-    });
-    res.json(result);
-})
+        })
+    ).body.hits.hits;
 
-const start = async () => {
+    const reformat = [];
+
+    for (const rs of result) {
+        reformat.push({
+            id: Number.parseInt(rs._id),
+            name: rs._source.name,
+            description: rs._source.description,
+        });
+    }
+
+    console.log(reformat);
+
+    if (result) {
+        callback(null, { products: reformat });
+    } else {
+        callback({
+            message: "Recipe not found",
+            code: grpc.status.INVALID_ARGUMENT,
+        });
+    }
+}
+
+
+const setupElasticSearch = async () => {
     await client.ping();
-    if (!isExistIndex('products')) {
+    if (!isExistIndex("products")) {
         await make_indexing("products");
     }
-   
+
     await addMapToIndex("products", productNormalProperties);
     for (const product of products) {
         const { name, description } = product;
-        await insertDoc("products", product.id.toString(), { name, description });
-    
+        await insertDoc("products", product.id.toString(), {
+            name,
+            description,
+        });
     }
-    console.log("started succesfully");
-    app.listen(3001, ()=> console.log('service elastic search running on port 3001'));
-};
-
-start();
+}
+const server = new grpc.Server();
+server.addService(productsProto.Products.service, { find: findProduct });
+server.bindAsync(
+    "0.0.0.0:50051",
+    grpc.ServerCredentials.createInsecure(),
+    async () => {
+        await setupElasticSearch();
+        console.log("es started succesfully");
+        server.start();
+        console.log("grpc running at 0.0.0.0:50051");
+    }
+);
